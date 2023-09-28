ef179175c6bd909132698a89c3a155549bd6d9b3
Kehinde Alaga <kehinde.alaga@globalaccelerex.com>
2023-09-28 14:41:33 +0100

    an article app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'article-app';
+
+  isShow: boolean = true;
+
+  onClick() {
+    this.isShow = !this.isShow;
+  }
 }
